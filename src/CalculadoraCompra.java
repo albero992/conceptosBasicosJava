ec4106baaaeddffe4327b77b68685c67f86d0f2b
Ricardo Albero Rico <albero992@gmail.com>
2025-05-23 22:30:10 +0200

    calculadora de descuentos realizada.

--- src/CalculadoraCompra.java
+++ src/CalculadoraCompra.java
@@ -2,32 +2,17 @@ public class CalculadoraCompra {
     public static void main(String[] args) {
 
         double precioOriginalProducto = 150.75;
-        double porcentajeDescuento = 0.20; // Esto representa un 20%
+        double porcentajeDescuento = 0.20;
         int cantidadProductos = 3;
         boolean esClientePremium = true;
         boolean tieneCupon = false;
-
-
         double montoDescuento = precioOriginalProducto * porcentajeDescuento;
-
-
         double precioConDescuentoUnitario = precioOriginalProducto - montoDescuento;
-
-
         double precioTotalCompra = precioConDescuentoUnitario * cantidadProductos;
-
-
-        double precioFinal = precioTotalCompra; // Inicializamos precioFinal con el total actual
-
-
+        double precioFinal = precioTotalCompra;
         System.out.println("No se aplicó descuento adicional (no es Premium o no tiene cupón).");
-
-
-
         double umbralEnvioGratis = 100.0;
         boolean envioGratis = precioFinal > umbralEnvioGratis;
-
-
         System.out.println("--- Detalles de la Compra ---");
         System.out.println("Precio original del producto: " + precioOriginalProducto + "€");
         System.out.println("Porcentaje de descuento: " + (porcentajeDescuento * 100) + "%");
