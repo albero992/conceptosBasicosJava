b85482c2ed1a750ec31f335d2aa8c8d66dbc95b8
Ricardo Albero Rico <albero992@gmail.com>
2025-05-23 21:11:04 +0200

    Initial commit

---
+++ src/PerfilUsuario.java
@@ -0,0 +1,14 @@
+public class PerfilUsuario {
+    public static void main(String []args){
+
+      String name = "Ricardo";
+      int edad = 44;
+      double altura = 1.80;
+      boolean estudiante = true;
+      char apellido = 'A';
+      System.out.println("Nombre " + name + apellido);
+      System.out.println("Edad " + edad);
+      System.out.println("Altura" + altura);
+      System.out.println("estudiante " + estudiante);
+   }
+}
